7e5de0e6f83d7717a03f3fbbd72c28e6f8a6f956
Xavier <xavier.rosengard@efrei.net>
2021-11-22 18:11:38 +0100

    Bonjour

---
+++ cmake-build-debug/CMakeFiles/ok.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ok.dir/Test.c.obj"
+  "libok.dll.a"
+  "ok.exe"
+  "ok.exe.manifest"
+  "ok.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ok.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
